6362fd4f71f4dccffcb543dd133b8e41f69fe9f6
julu <juanluismatalop@gmail.com>
2024-11-12 20:44:34 +0100

    commit inicial

---
+++ Trasferencia.java
@@ -0,0 +1,16 @@
+package Ejercicio5_PSP;
+
+public class Trasferencia {
+    public static boolean transferencia(Cuenta c1, Cuenta c2, double cantidad) {
+        synchronized (c1) {
+            if (c1.getSaldo() < cantidad) {
+                return false;
+            }
+            c1.sacarCantidad(cantidad);
+            synchronized (c2) {
+                c2.ingresaCantidad(cantidad);
+            }
+        }
+        return true;
+    }
+}
